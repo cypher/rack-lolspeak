726917ca8ab34645883ac8a5915ad5cbddb1f162
Markus Prinz <markus.prinz@nuclearsquid.com>
2009-04-08 21:22:51 +0200

    Add test for text/plain

--- test/spec_rack_icanhazlolcode.rb
+++ test/spec_rack_icanhazlolcode.rb
@@ -1,4 +1,16 @@
 require 'test/spec'
 require 'rack/mock'
-require 'rack/contrib/icanhazlolcodeplzkbykthx'
+require 'rack/contrib/icanhazlolcode'
 
+context "Rack::Contrib::ICanHazLOLCode" do
+  context "text/plain" do
+    app = lambda {|env| [200, {'Content-Type' => 'text/plain'}, "Hello, kitty"] }
+
+    specify "should convert the body to lolspeak" do
+      status, headers, body = Rack::Contrib::ICanHazLOLCode.new(app).call({})
+
+      status.should.equal 200
+      body.should.equal "y halo thar, kitty"
+    end
+  end
+end
