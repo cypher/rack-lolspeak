18b21c7ba752080c75c83467c764178db49bdb5a
Markus Prinz <markus.prinz@nuclearsquid.com>
2009-04-08 21:15:58 +0200

    Naive version of lolspeak conversion

--- lib/rack/contrib/icanhazlolcode.rb
+++ lib/rack/contrib/icanhazlolcode.rb
@@ -10,6 +10,11 @@ module Rack
       def call(env)
         status, headers, body = @app.call(env)
 
+        if headers['Content-Type'] == 'text/html'
+          body = body.to_lolspeak
+          headers['Content-Length'] = body.size.to_s
+        end
+
         [status, headers, body]
       end
     end
